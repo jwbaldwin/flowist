e75a31589bf505d546ff395daa16adeb58aae87b
James Baldwin <jbaldwin@Quantum-book.local>
2018-12-01 00:11:00 -0500

    initial

---
+++ src/main/java/com/jbaldwin/novu/NovuApplication.java
@@ -0,0 +1,12 @@
+package com.jbaldwin.novu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jbaldwin/novu/NovuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jbaldwin.novu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NovuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
