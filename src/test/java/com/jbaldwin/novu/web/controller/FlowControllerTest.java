cacae538eb178133ebc0f414193be544dc3b6de0
James Baldwin <jwbaldwin3@aol.com>
2019-02-24 15:41:15 -0500

    commiting before working on jar

---
+++ src/test/java/com/jbaldwin/novu/web/controller/FlowControllerTest.java
@@ -0,0 +1,23 @@
+package com.jbaldwin.novu.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FlowControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getFlowById() {
+    }
+
+    @Test
+    public void getAllFlows() {
+    }
+
+    @Test
+    public void saveFlow() {
+    }
+}
